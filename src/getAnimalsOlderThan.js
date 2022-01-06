fd026ad814e0cf1fd9c2febc6a25d471292cba00
eduardobazler <edubazler@gmail.com>
2022-01-06 16:10:46 -0300

    segundo requisito adicionado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+const { species } = data;
+
+function getAnimalsOlderThan(animal, ageFunction) {
+  const currentAnimal = species.find(({ name }) => name === animal);
+  const { residents } = currentAnimal;
+  return residents.every(({ age }) => age >= ageFunction);
 }
 
 module.exports = getAnimalsOlderThan;

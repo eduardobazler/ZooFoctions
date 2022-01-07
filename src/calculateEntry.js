2d81210fab88d3e10e4cd0ce881aa0cc9e6022a6
eduardobazler <edubazler@gmail.com>
2022-01-07 16:27:37 -0300

    sexto requisito adicionado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-
+function countEntrants(entrants = []) {
+  if (entrants !== undefined) {
+    try {
+      const numberChild = entrants.filter(({ age }) => age < 18).length;
+      const numberAdult = entrants.filter(({ age }) => age >= 18 && age < 50).length;
+      const numberSenior = entrants.filter(({ age }) => age >= 50).length;
+      return { child: numberChild, adult: numberAdult, senior: numberSenior };
+    } catch (err) {
+      return { child: 0, adult: 0, senior: 0 };
+    }
+  }
+  return { child: 0, adult: 0, senior: 0 };
 }
 
+const priceChild = 20.99;
+const priceAdult = 49.99;
+const priceSenior = 24.99;
+
 function calculateEntry(entrants) {
-  // seu código aqui
+  const { child, adult, senior } = countEntrants(entrants);
+  const conta = (child * priceChild) + (adult * priceAdult) + (senior * priceSenior);
+  return conta;
 }
 
 module.exports = { calculateEntry, countEntrants };

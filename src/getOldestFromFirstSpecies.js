ebe862819b1ecdfa8667b943dd197fe8fc34fa93
eduardobazler <edubazler@gmail.com>
2022-01-10 10:39:25 -0300

    nono requerimento adicionado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,21 @@
+/* const { animal } = require('faker'); */
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+const { species, employees } = data;
+
+function getOldReduce(acc, curret) {
+  if (curret.age > acc.age) {
+    return curret;
+  }
+  return acc;
+}
+
+function getOldestFromFirstSpecies(idParameter) {
+  const { responsibleFor: listOfIdAnimals } = employees.find(({ id }) => id === idParameter);
+  const animalReturnId = listOfIdAnimals[0];
+  const { residents } = species.find(({ id }) => id === animalReturnId);
+  const { name, sex, age } = residents.reduce(getOldReduce);
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
